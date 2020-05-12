b3469fbc9bb4c4501ccf7212ab2627155a53d449
ziyan <46317855+ziyanfadilla@users.noreply.github.com>
2020-05-12 06:11:39 -0700

    p07- Big Data

---
+++ src/mapreduce/Koneksi.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapreduce;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ *
+ * @author asus
+ */
+public class Koneksi {
+    public static  MongoDatabase sambungDB(){
+        try{
+            MongoClient client = MongoClients.create();
+            MongoDatabase database = client.getDatabase("bigdata");
+            return database;
+        } catch (Exception e){
+            System.out.println("Koneksi Gagal: "+ e.getMessage());
+        }
+        return null;
+    }
+    
+}
